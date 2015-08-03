9c04beb6c2cc15c31346e78d42958b8130136f20
Cassiano D'Andrea <cassiano.dandrea@tagview.com.br>
2015-08-03 11:50:26 -0300

    Refatorando parsing dos JSONs

--- client/app/controllers/tasks_controller.rb
+++ client/app/controllers/tasks_controller.rb
@@ -1,10 +1,10 @@
 class TasksController < ApplicationController
   def show
     if logged_in?
-      @tasks = JSON.parse(access_token.get('api/tasks.json').body).map do |task|
-        task['due_date'] = DateTime.parse(task['due_date']) if task['due_date']
-
-        OpenStruct.new task
+      @tasks = JSON.parse(access_token.get('api/tasks.json').body, object_class: OpenStruct).map do |task|
+        task.tap do |t|
+          t.due_date = DateTime.parse(t.due_date) if t.due_date
+        end
       end
     end
   end
