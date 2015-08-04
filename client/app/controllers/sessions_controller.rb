20096736ec143b93d28fcc084720a9358c0e30da
Cassiano D'Andrea <cassiano.dandrea@tagview.com.br>
2015-08-04 11:33:25 -0300

    Exibindo falha de autenticação (acesso negado) do provedor de OAuth

--- client/app/controllers/sessions_controller.rb
+++ client/app/controllers/sessions_controller.rb
@@ -1,13 +1,17 @@
 class SessionsController < ApplicationController
   def callback
-    oauth2_token           = oauth2_client.auth_code.get_token(params[:code], redirect_uri: OAUTH2_PROVIDER[:callback])
-    session[:oauth2_token] = oauth2_token.token
-
-    if params[:state] && (return_to_uri = decode_data(params[:state])['return_to_uri']) =~ URI::ABS_URI
-      redirect_to return_to_uri
+    if params[:error] && params[:error] == 'access_denied'
+      render plain: params[:error_description]
     else
-      # Display the generated token, for debugging purposes only (could optionally redirect to a default page).
-      render plain: oauth2_token.token
+      oauth2_token           = oauth2_client.auth_code.get_token(params[:code], redirect_uri: OAUTH2_PROVIDER[:callback])
+      session[:oauth2_token] = oauth2_token.token
+
+      if params[:state] && (return_to_uri = decode_data(params[:state])['return_to_uri']) =~ URI::ABS_URI
+        redirect_to return_to_uri
+      else
+        # Display the generated token, for debugging purposes only (could optionally redirect to a default page).
+        render plain: oauth2_token.token
+      end
     end
   end
 
