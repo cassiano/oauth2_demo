efea1d4f2d1c2d5e85c0315059a78c944a3d7675
Cassiano D'Andrea <cassiano.dandrea@tagview.com.br>
2015-08-17 14:49:56 -0300

    Usando gem `roar` para representação dos JSONs

--- server/app/controllers/api/v2/users.rb
+++ server/app/controllers/api/v2/users.rb
@@ -1,13 +1,30 @@
+require 'roar/json'
+
 module API
   module V2
+    module UserRepresenter
+      include Roar::JSON
+      include Roar::Hypermedia
+
+      property :email
+
+      link :tasks do
+        'api/v2/tasks.json'
+      end
+
+      link :search do
+        'api/v2/tasks/search.json'
+      end
+    end
+
     class Users < Grape::API
       include API::V2::Defaults
 
       resource :users do
-        desc "Return current user"
+        desc 'Return current user'
         oauth2
         get :whoami do
-          resource_owner.tap do |user|
+          resource_owner.extend(UserRepresenter).tap do |user|
             user.email.reverse!
           end
         end
